07d951579f99c0bf62db255b4aec838b9392affa
Adam Cichy <adam.cichyy3@gmail.com>
2018-05-01 08:42:01 +0200

    Dodanie materiałów i nowych pickUpów

--- Assets/Scripts/RotatingPickUp.cs
+++ Assets/Scripts/RotatingPickUp.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotatingCube : MonoBehaviour {
+public class RotatingPickUp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
